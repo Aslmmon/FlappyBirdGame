8789c445d9c3aa0278d63c3f2bcc136cd6384db3
Aslm <aslmmon993@gmail.com>
2024-04-04 11:54:45 -0300

    added animation for main screen

--- lib/screens/main_menu.dart
+++ lib/screens/main_menu.dart
@@ -3,21 +3,48 @@ import 'package:flappybird/flappyGame/flabby_bird_game.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MainMenu extends StatelessWidget {
+class MainMenu extends StatefulWidget {
   final FlappyBirdGame flappyBirdGame;
   static const String id = "mainMenu";
 
   const MainMenu({super.key, required this.flappyBirdGame});
 
   @override
-  Widget build(BuildContext context) {
-    flappyBirdGame.pauseEngine();
+  State<MainMenu> createState() => _MainMenuState();
+}
 
+class _MainMenuState extends State<MainMenu> with TickerProviderStateMixin {
+  late final AnimationController _controller;
+  late final Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    widget.flappyBirdGame.pauseEngine();
+
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..forward();
+
+    _animation = CurvedAnimation(
+      parent: _controller,
+      curve: Curves.fastOutSlowIn,
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget build(BuildContext context) {
     return Scaffold(
       body: GestureDetector(
         onTap: () {
-          flappyBirdGame.overlays.remove(id);
-          flappyBirdGame.resumeEngine();
+          widget.flappyBirdGame.overlays.remove(MainMenu.id);
+          widget.flappyBirdGame.resumeEngine();
         },
         child: Container(
           width: double.infinity,
@@ -25,7 +52,8 @@ class MainMenu extends StatelessWidget {
           decoration: const BoxDecoration(
               image: DecorationImage(
                   image: AssetImage(Assets.menu), fit: BoxFit.cover)),
-          child: Image.asset(Assets.message),
+          child: ScaleTransition(
+              scale: _animation, child: Image.asset(Assets.message)),
         ),
       ),
     );
